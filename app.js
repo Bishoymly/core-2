5f89c648cd39f034dc6a2e595957fadf91d2b3e0
Bishoy Labib <Bishoy.Labib@integrant.com>
2022-11-12 17:21:23 -0800

    added react app

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-const CosmosClient = require('@azure/cosmos').CosmosClient
-const config = require('./config')
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+const config = require("./config");
 
-const express = require('express')
-const path = require('path')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const swaggerUI = require("swagger-ui-express");
 const Schema = require("./core/schema");
-const { response } = require('express')
+const { response } = require("express");
+const designerRouter = require("./routes/designer");
 
-const app = express()
+const app = express();
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
- // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/designer", designerRouter);
+app.use(express.static(path.join(__dirname, "build")));
 
- // uncomment after placing your favicon in /public
- //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
- app.use(logger('dev'))
- app.use(bodyParser.json())
- app.use(bodyParser.urlencoded({ extended: false }))
- app.use(cookieParser())
- app.use(express.static(path.join(__dirname, 'public')))
+Schema.addRoutes(app);
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(Schema.getSwaggerUIDocs())
+);
 
- 
+//app.set('view engine', 'jade')
 
- Schema.addRoutes(app);
- app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(Schema.getSwaggerUIDocs()));
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
- //app.set('view engine', 'jade')
-
- // catch 404 and forward to error handler
- app.use(function(req, res, next) {
-   const err = new Error('Not Found')
-   err.status = 404
-   next(err)
- })
-
- // error handler
- app.use(function(err, req, res, next) {
-  console.error(err.stack); 
-  // only providing error in development   
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  // only providing error in development
   const response = {
-    error: req.app.get('env') === 'development' ? err.stack : err.message
+    error: req.app.get("env") === "development" ? err.stack : err.message,
   };
-  
-  res.status(err.status || 500)
-  res.json(response)
- })
 
- module.exports = app
+  res.status(err.status || 500);
+  res.json(response);
+});
+
+module.exports = app;
--- core/schema.js
+++ core/schema.js
@@ -1,129 +1,134 @@
 // @ts-check
-const fs = require('fs');
-const path = require('path')
-const CosmosClient = require('@azure/cosmos').CosmosClient
-const config = require('../config')
-const debug = require('debug')
-const CrudService = require('./crud-service');
+const fs = require("fs");
+const path = require("path");
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+const config = require("../config");
+const debug = require("debug");
+const CrudService = require("./crud-service");
 
 class Schema {
-
   types = [];
 
   constructor() {
-    const jsonsInDir = fs.readdirSync('./schema').filter(file => path.extname(file) === '.json');
-    jsonsInDir.forEach(file => {
-        const fileData = fs.readFileSync(path.join('./schema', file));
-        const json = JSON.parse(fileData.toString());
-        this.types.push(json);
-      });
+    const jsonsInDir = fs
+      .readdirSync("./schema")
+      .filter((file) => path.extname(file) === ".json");
+    jsonsInDir.forEach((file) => {
+      const fileData = fs.readFileSync(path.join("./schema", file));
+      const json = JSON.parse(fileData.toString());
+      this.types.push(json);
+    });
   }
 
-  async addRoutes(app){
+  async addRoutes(app) {
     const cosmosClient = new CosmosClient({
-        endpoint: config.host,
-        key: config.authKey
-    })
-    
-    this.crudService = new CrudService(cosmosClient, config.databaseId, config.containerId, app, this.types);
-    await this.crudService.init()
+      endpoint: config.host,
+      key: config.authKey,
+    });
+
+    this.crudService = new CrudService(
+      cosmosClient,
+      config.databaseId,
+      config.containerId,
+      app,
+      this.types
+    );
+    await this.crudService.init();
   }
 
   getSwaggerUIDocs() {
-    
     const header = {
-        openapi: "3.0.3", // present supported openapi version
-        info: {
-          title: "Core 2.0 API", // short title.
-          description: "Core 2.0 API", //  desc.
-          version: "1.0.0", // version number
-        },
-      };
+      openapi: "3.0.3", // present supported openapi version
+      info: {
+        title: "Core 2.0 API", // short title.
+        description: "Core 2.0 API", //  desc.
+        version: "1.0.0", // version number
+      },
+    };
 
-      const components = {};
-      const tags = [];
-      const paths = {};
-      this.types.filter(t=>t.type==="Object").forEach(t => {
+    const components = {};
+    const tags = [];
+    const paths = {};
+    this.types
+      .filter((t) => t.type === "Object")
+      .forEach((t) => {
         const properties = {};
-        if(t.properties){
-            t.properties.forEach(p => {
-                properties[p.name]=this.toSwaggerProperty(t, p);
-            });
+        if (t.properties) {
+          t.properties.forEach((p) => {
+            properties[p.name] = this.toSwaggerProperty(t, p);
+          });
         }
         components[t.name] = {
           type: "object",
           properties: {
-            ...properties
-          }
+            ...properties,
+          },
         };
 
-        if(t.api==="CRUD"){
+        if (t.api === "CRUD") {
           this.crudService?.setupSwagger(t, tags, paths);
         }
       });
-    
-      const docs = {
-        ...header,
-        components: {
-            schemas: {
-                ...components
-            }
+
+    const docs = {
+      ...header,
+      components: {
+        schemas: {
+          ...components,
         },
-        tags: tags,
-        paths: paths
-      };
-      return docs;
+      },
+      tags: tags,
+      paths: paths,
+    };
+    return docs;
   }
 
-  toSwaggerProperty(t, p){
-    
+  toSwaggerProperty(t, p) {
     const result = {
       type: p.type,
-      example: ''
+      example: "",
     };
 
     // find if this property is of a another core type
     const t2 = this.findType(p.type);
-    if(t2){
-      if(t2.type === "Object"){
-        result['$ref'] = '#/components/schemas/' + t2.name;
-      }
-      else if(t2.type === "Lookup"){
-        result.type = 'String';
-        if(t2.values){
-          result.enum = t2.values.map(v=>v.code);
+    if (t2) {
+      if (t2.type === "Object") {
+        result["$ref"] = "#/components/schemas/" + t2.name;
+      } else if (t2.type === "Lookup") {
+        result.type = "String";
+        if (t2.values) {
+          result.enum = t2.values.map((v) => v.code);
         }
-      }
-      else{
+      } else {
         result.type = t2.type;
-      }      
-      
-      if(t2.example){
+      }
+
+      if (t2.example) {
         result.example = t2.example;
       }
 
-      if(t2.display){
+      if (t2.display) {
         result.display = t2.display;
       }
     }
 
-    if(p.display){
+    if (p.display) {
       result.display = p.display;
     }
 
-    if(p.example){
+    if (p.example) {
       result.example = p.example;
     }
 
-    if(p.required){
+    if (p.required) {
       result.required = p.required;
     }
 
     return result;
   }
 
-  findType(name){
-    return this.types.find(type=>type.name===name);
+  findType(name) {
+    return this.types.find((type) => type.name === name);
   }
 }
 
